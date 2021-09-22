e4764ad16519eb981f3dbe898efd09ac2ccb0f69
DennisY <yliu3343@gmail.com>
2021-09-22 15:27:35 +1200

    table

---
+++ src/components/ScheduleForms.jsx
@@ -0,0 +1,15 @@
+import React,{useState} from "react";
+import { Table, Column, HeaderCell, Cell } from "rsuite-table";
+
+const GoalsForm =()=> {
+    return (
+    <>
+        <article>
+
+        </article>
+
+
+    </>
+    );
+    
+}
