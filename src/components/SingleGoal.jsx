2fa034f66addf83fe49421aae7e4a9a1a6abd232
Jackli-Tech <cooperationjackli@gmail.com>
2021-09-26 22:24:52 +1300

    show data version1

--- src/components/SingleGoal.jsx
+++ src/components/SingleGoal.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 
-export default function SingleGoal({ singleGoal, handleGoal, handleTask }) {
+export default function SingleGoal({
+  singleGoal,
+  handleGoal,
+  handleTask,
+  defaultValue,
+  taskInputName,
+  activityDefaultValue,
+  activityInputName,
+}) {
   return (
     <li className="table-row">
       <div className="col col-1">{singleGoal.goalName}</div>
       <div className="col col-2" data-label="Task">
         <input
           type="text"
-          name="taskName"
-          id="taskName"
-          value={singleGoal.taskName}
+          name={taskInputName}
+          id={taskInputName}
           placeholder="Asking questions"
+          defaultValue={defaultValue}
           onChange={handleTask}
         />
       </div>
       <div className="col col-3" data-label="Activity">
         <input
           type="text"
-          name="activity"
-          id="activity"
-          value={singleGoal.activity}
+          name={activityInputName}
+          id={activityInputName}
           placeholder="Mentoring"
+          defaultValue={activityDefaultValue}
           onChange={handleTask}
         />
       </div>
--- src/pages/ScheduleFormPage.jsx
+++ src/pages/ScheduleFormPage.jsx
@@ -4,26 +4,56 @@ export default function ScheduleFormPage() {
   const [goalsList, setGoalsList] = useState([]);
   const [singleGoal, setSingleGoal] = useState({
     goalName: "Understand the norms",
-    taskName: "",
-    activity: "",
+    taskName: "Peer Support",
+    activity: "Training",
+  });
+  const [secondGoal, setsecondGoal] = useState({
+    goalName: "Knowing git",
+    taskName1: "",
+    activity1: "",
+  });
+  const [thirdGoal, setthirdGoal] = useState({
+    goalName: "Knowing React",
+    taskName2: "",
+    activity2: "",
+  });
+  const [forthGoal, setforthGoal] = useState({
+    goalName: "Knowing Jest",
+    taskName3: "",
+    activity3: "",
+  });
+  const [fifthGoal, setfifthGoal] = useState({
+    goalName: "Knowing Java",
+    taskName4: "",
+    activity4: "",
   });
   function handleGoal(rowGoal) {
     console.log(rowGoal);
     const newSingleGoal = {
       ...rowGoal,
-      id: new Date().getTime().toString(),
     };
     setGoalsList((prev) => {
-      return [...prev, newSingleGoal];
+      console.log([...prev, rowGoal]);
+      return [...prev, rowGoal];
     });
-    console.log(goalsList);
   }
 
   function handleChange(event) {
     const name = event.target.name;
     const value = event.target.value;
-    console.log(name + event.target.value);
-    setSingleGoal({ ...singleGoal, [name]: value });
+    console.log(name + value);
+    if (name === "taskName" || name === "activity") {
+      setSingleGoal({ ...singleGoal, [name]: value });
+    } else if (name === "taskName1" || name === "activity1") {
+      console.log("taskname111111");
+      setsecondGoal({ ...secondGoal, [name]: value });
+    } else if (name === "taskName2" || name === "activity2") {
+      setthirdGoal({ ...thirdGoal, [name]: value });
+    } else if (name === "taskName3" || name === "activity3") {
+      setforthGoal({ ...forthGoal, [name]: value });
+    } else if (name === "taskName4" || name === "activity4") {
+      setfifthGoal({ ...fifthGoal, [name]: value });
+    }
   }
   return (
     <div>
@@ -37,9 +67,49 @@ export default function ScheduleFormPage() {
             <div className="col col-4">Choose</div>
           </li>
           <SingleGoal
+            taskInputName="taskName"
+            activityInputName="activity"
             singleGoal={singleGoal}
             handleGoal={handleGoal}
             handleTask={handleChange}
+            defaultValue="Peer Support"
+            activityDefaultValue="Training"
+          />
+          <SingleGoal
+            taskInputName="taskName1"
+            activityInputName="activity1"
+            singleGoal={secondGoal}
+            handleGoal={handleGoal}
+            handleTask={handleChange}
+            defaultValue="Induction"
+            activityDefaultValue="Pair Programming"
+          />
+          <SingleGoal
+            taskInputName="taskName2"
+            activityInputName="activity2"
+            singleGoal={thirdGoal}
+            handleGoal={handleGoal}
+            handleTask={handleChange}
+            defaultValue="Team Leader Assistance"
+            activityDefaultValue="Mentoring"
+          />
+          <SingleGoal
+            taskInputName="taskName3"
+            activityInputName="activity3"
+            singleGoal={forthGoal}
+            handleGoal={handleGoal}
+            handleTask={handleChange}
+            defaultValue="Team Leader Assistance"
+            activityDefaultValue="Mentoring"
+          />
+          <SingleGoal
+            taskInputName="taskName4"
+            activityInputName="activity4"
+            singleGoal={fifthGoal}
+            handleGoal={handleGoal}
+            handleTask={handleChange}
+            defaultValue="Team Leader Assistance"
+            activityDefaultValue="Mentoring"
           />
         </ul>
       </div>
@@ -55,8 +125,20 @@ export default function ScheduleFormPage() {
             return (
               <li className="table-row" key={index}>
                 <div className="col col-1">{goal.goalName}</div>
-                <div className="col col-2">{goal.taskName}</div>
-                <div className="col col-3">{goal.activity}</div>
+                <div className="col col-2">
+                  {singleGoal.taskName ||
+                    goal.taskName1 ||
+                    goal.taskName2 ||
+                    goal.taskName3 ||
+                    goal.taskName4}
+                </div>
+                <div className="col col-3">
+                  {goal.activity ||
+                    goal.activity1 ||
+                    goal.activity2 ||
+                    goal.activity3 ||
+                    goal.activity4}
+                </div>
               </li>
             );
           })}
