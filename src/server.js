a7eef493885efb220637493ac7f240beef5f216c
Jackli-Tech <cooperationjackli@gmail.com>
2021-10-10 20:54:22 +1300

    6

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+// const schduleFormRoutes = require("../backend/routes/SchduleForm");
+const connectDB = require("../backend/config/db");
+const app = express();
+
+// connectDB();
+app.use(express.json());
+// app.use("/scheduleform/form", schduleFormRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`welcome to port${PORT}`));
