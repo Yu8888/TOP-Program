906253742b14d698cc243a1bd87221f89cac1b16
Jackli-Tech <cooperationjackli@gmail.com>
2021-10-10 20:48:20 +1300

    try

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-// const express = require("express");
-// const dotenv = require("dotenv");
+const express = require("express");
+
 // const schduleFormRoutes = require("../backend/routes/SchduleForm");
-// const connectDB = require("./config/db");
-// const app = express();
-// dotenv.config();
+const connectDB = require("./config/db");
+const app = express();
+
 // connectDB();
-// app.use(express.json());
+app.use(express.json());
 // app.use("/scheduleform/form", schduleFormRoutes);
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`welcome to port${PORT}`));
+const PORT = 5000;
+app.listen(PORT, () => console.log(`welcome to port${PORT}`));
